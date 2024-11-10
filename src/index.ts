055914e6cd6f503dd0dcc870f7c0161236bc8bee
Chimder <dimas.kirilyuk@gmail.com>
2024-11-10 21:17:46 +0200

    fix headers

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,36 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { handle } from "hono/vercel";
 
 const app = new Hono();
 
-app.use("/api/proxy", cors());
+// app.use("/api/proxy", cors());
 
 app.get("/api/proxy", async (c) => {
   const url = c.req.query("url");
 
-  if (!url) {
-    return c.json({ error: "URL is required" }, 400);
+  if (!url || !url.startsWith("https://")) {
+    return c.json({ error: "URL must use HTTPS" }, 400);
   }
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "YourCustomUserAgent/1.0",
+        Accept: "image/*",
+        "Accept-Encoding": "gzip, deflate, br",
+      },
+    });
+    console.log("STARUS", response);
+    if (response.status >= 300 && response.status < 400) {
+      return c.json(
+        { error: `Redirected to ${response.headers.get("Location")}` },
+        500
+      );
+    }
+
+    if (!response.ok) {
+      return c.json({ error: `Failed with status ${response.status}` }, 500);
+    }
 
     if (!response.ok) {
       console.error(
