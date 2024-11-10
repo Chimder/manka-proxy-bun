a169f719d31371e7343b23db7c6ae18aa27b31bf
Chimder <dimas.kirilyuk@gmail.com>
2024-11-10 18:37:04 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -10,21 +10,17 @@ app.get("/api/proxy", async (c) => {
   }
 
   try {
-    // Получаем изображение с внешнего URL с помощью fetch
     const response = await fetch(url);
 
     if (!response.ok) {
       return c.json({ error: "Failed to fetch image" }, 500);
     }
 
-    // Извлекаем заголовок Content-Type из ответа
     const contentType =
       response.headers.get("content-type") || "application/octet-stream";
 
-    // Чтение данных изображения в виде массива байтов
     const imageData = await response.arrayBuffer();
 
-    // Возвращаем изображение с корректным заголовком Content-Type
     return c.body(imageData, 200, { "Content-Type": contentType });
   } catch (error) {
     return c.json({ error: "Failed to fetch image" }, 500);
